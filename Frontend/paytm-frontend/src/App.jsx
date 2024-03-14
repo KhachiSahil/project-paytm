731d64e29309141f51ef7aae7722d68a621988a3
Sahil Khachi <sahilkhachi95@gmail.com>
2024-03-14 19:24:45 +0530

    Frontend started Routes setup

---
+++ Frontend/paytm-frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Signin from './component/Signin';
+import Signup from './component/Signup';
+import Dashboard from './component/Dashboard';
+import Send from './component/Send';
+
+function App() {
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+          <Route path='/signin' element = {<Signin/>}/>
+          <Route path='/signup' element = {<Signup/>} />
+          <Route path='/dashboard' element = {<Dashboard/>}/>
+          <Route path='/send' element= {<Send/>}/>
+      </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
